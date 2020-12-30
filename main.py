b5100144571a37d0824b50476f972902c31372e4
misha_blin <wildline007@gmail.com>
2020-12-31 01:40:52 +0500

    Items were added Animations were added
    SNAIL SIMULATOR 0.2 alpha

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
-import pygame as pg
 import sys
-from settings import *
 from sprites import *
 from map_render import *
 from os import *
+import random
+
+
+def load_images_from_folder(folder):
+    images = []
+    for filename in listdir(folder):
+        img = pg.image.load(path.join(folder, filename))
+        images.append(pg.transform.scale(img, (32, 32)))
+    return images
 
 
 class Game:
     def __init__(self):
         pg.init()
-        self.screen = pg.display.set_mode((WIDTH, HEIGHT))
+        self.screen = pg.display.set_mode((WIDTH, HEIGHT), pg.FULLSCREEN)
         pg.display.set_caption(TITLE)
         self.clock = pg.time.Clock()
         pg.key.set_repeat(500, 100)
@@ -18,19 +25,45 @@ class Game:
     def load_data(self):
         game_folder = path.dirname(__file__)
         map_folder = path.join(game_folder, 'map')
+        self.sprites_folder = path.join(game_folder, 'sprites')
+
         self.map = TiledMap(path.join(map_folder, 'map.tmx'))
         self.map_image = self.map.make_map()
         self.map_rect = self.map_image.get_rect()
-        self.player_image = pg.image.load(PLAYER_IMAGE).convert_alpha()
+
+        self.load_player_sprites()
+
+        self.item_images = {}
+        for item in ITEM_IMAGES:
+            self.item_images[item] = pg.transform.scale(
+                pg.image.load(path.join(self.sprites_folder, ITEM_IMAGES[item])).convert_alpha(), (20, 20))
+
+    def load_player_sprites(self):
+        snail_folder = path.join(self.sprites_folder, 'snail_images')
+        self.standing_animation = load_images_from_folder(path.join(snail_folder, 'standing'))
+        self.moving_forward = load_images_from_folder(path.join(snail_folder, 'moving_forward'))
+        self.moving_down = load_images_from_folder(path.join(snail_folder, 'moving_down'))
+        self.moving_right = load_images_from_folder(path.join(snail_folder, 'moving_right'))
+        self.moving_left = load_images_from_folder(path.join(snail_folder, 'moving_left'))
+        self.moving_down_right = load_images_from_folder(path.join(snail_folder, 'moving_down_right'))
+        self.moving_down_left = load_images_from_folder(path.join(snail_folder, 'moving_down_left'))
+        self.moving_left_up = load_images_from_folder(path.join(snail_folder, 'moving_forward_left'))
+        self.moving_right_up = load_images_from_folder(path.join(snail_folder, 'moving_forward_right'))
+        self.death_animation = load_images_from_folder(path.join(snail_folder, 'death'))
+        self.housing_animation = load_images_from_folder(path.join(snail_folder, 'housing_animation'))
+        self.reversed_housing = self.housing_animation[::-1]
+        self.house = load_images_from_folder(path.join(snail_folder, 'house'))
 
     def new(self):
         self.all_sprites = pg.sprite.Group()
         self.walls = pg.sprite.Group()
+        self.items = pg.sprite.Group()
         for tile_object in self.map.tmxdata.objects:
             if tile_object.name == 'player':
                 self.player = Player(self, tile_object.x, tile_object.y)
             if tile_object.name == 'obstacle':
                 Obstacle(self, tile_object.x, tile_object.y, tile_object.width, tile_object.height)
+        Item(self, (random.randrange(1, self.map.width), random.randrange(1, self.map.height)), 'apple')
         self.camera = Camera(self.map.width, self.map.height)
 
     def run(self):
@@ -48,6 +81,13 @@ class Game:
     def update(self):
         self.all_sprites.update()
         self.camera.update(self.player)
+        hits = pg.sprite.spritecollide(self.player, self.items, True)
+        for hit in hits:
+            if hit.type == 'apple':
+                hit.kill()
+                self.player.add_speed(SPEED_BY_APPLE)
+                # Item(self, (random.randrange(1, self.map.width), random.randrange(1, self.map.height)), 'apple')
+                Item(self, (random.randrange(1, self.map.width), random.randrange(1, self.map.height)), 'apple')
 
     def draw_grid(self):
         for x in range(0, WIDTH, TILESIZE):
--- sprites.py
+++ sprites.py
@@ -1,5 +1,6 @@
 import pygame as pg
 from settings import *
+import pytweening as tween
 
 vec = pg.math.Vector2
 
@@ -9,23 +10,113 @@ class Player(pg.sprite.Sprite):
         self.groups = game.all_sprites
         pg.sprite.Sprite.__init__(self, self.groups)
         self.game = game
-        self.image = game.player_image
+
+        self.animation = 'standing'
+        self.animation_standing = game.standing_animation
+        self.animation_right = game.moving_right
+        self.animation_left = game.moving_left
+        self.animation_forward = game.moving_forward
+        self.animation_forward_right = game.moving_right_up
+        self.animation_forward_left = game.moving_left_up
+        self.animation_down_left = game.moving_down_left
+        self.animation_down = game.moving_down
+        self.animation_death = game.death_animation
+        self.animation_down_right = game.moving_down_right
+        self.house_animation = game.house
+        self.housing_animation = game.housing_animation
+        self.reversed_housing = game.reversed_housing
+        self.current_sprite = 0
+        self.image = self.animation_right[self.current_sprite]
+        self.animate = True
+        self.in_house = False
+        self.w = True
+        self.d = True
+        self.a = True
+        self.s = True
+
         self.rect = self.image.get_rect()
         self.vel = vec(0, 0)
         self.y = y
         self.pos = vec(x, y)
+        self.speed = PLAYER_SPEED
 
     def get_keys(self):
+        if not self.in_house:
+            self.w = True
+            self.d = True
+            self.a = True
+            self.s = True
         self.vel = vec(0, 0)
         keys = pg.key.get_pressed()
-        if keys[pg.K_LEFT] or keys[pg.K_a]:
-            self.vel.x = -PLAYER_SPEED
-        if keys[pg.K_RIGHT] or keys[pg.K_d]:
-            self.vel.x = PLAYER_SPEED
-        if keys[pg.K_DOWN] or keys[pg.K_s]:
-            self.vel.y = PLAYER_SPEED
-        if keys[pg.K_UP] or keys[pg.K_w]:
-            self.vel.y = -PLAYER_SPEED
+        if not self.in_house:
+            if keys[pg.K_w] and keys[pg.K_d]:
+                self.w = False
+                self.d = False
+                self.vel.x = self.speed
+                self.vel.y = -self.speed
+                self.animate = True
+                self.animation = 'forward_right'
+        if not self.in_house:
+            if keys[pg.K_a] and keys[pg.K_w]:
+                self.w = False
+                self.a = False
+                self.vel.x = -self.speed
+                self.vel.y = -self.speed
+                self.animate = True
+                self.animation = 'forward_left'
+        if not self.in_house:
+            if keys[pg.K_a] and keys[pg.K_s]:
+                self.s = False
+                self.a = False
+                self.vel.x = -self.speed
+                self.vel.y = self.speed
+                self.animate = True
+                self.animation = 'down_left'
+
+        if not self.in_house:
+            if keys[pg.K_d] and keys[pg.K_s]:
+                self.s = False
+                self.d = False
+                self.vel.y = self.speed
+                self.vel.x = self.speed
+                self.animate = True
+                self.animation = 'down_right'
+
+        if self.a:
+            if keys[pg.K_LEFT] or keys[pg.K_a]:
+                self.vel.x = -self.speed
+                self.animate = True
+                self.animation = 'left'
+
+        if self.d:
+            if keys[pg.K_RIGHT] or keys[pg.K_d]:
+                self.vel.x = self.speed
+                self.animate = True
+                self.animation = 'right'
+        if self.s:
+            if keys[pg.K_DOWN] or keys[pg.K_s]:
+                self.vel.y = self.speed
+                self.animate = True
+                self.animation = 'down'
+        if self.w:
+            if keys[pg.K_UP] or keys[pg.K_w]:
+                self.vel.y = -self.speed
+                self.animate = True
+                self.animation = 'forward'
+
+        if keys[pg.K_SPACE]:
+            pg.time.wait(100)
+            self.in_house = not self.in_house
+            if self.in_house:
+                self.w = False
+                self.s = False
+                self.a = False
+                self.d = False
+                self.vel.y = 0
+                self.vel.x = 0
+                self.animate = True
+                self.animation = 'housing'
+
         if self.vel.x != 0 and self.vel.y != 0:
             self.vel *= 0.7071
 
@@ -49,6 +140,9 @@ class Player(pg.sprite.Sprite):
                 self.vel.y = 0
                 self.rect.y = self.pos.y
 
+    def add_speed(self, amount):
+        self.speed += amount
+
     def update(self):
         self.get_keys()
         self.pos += self.vel * self.game.dt
@@ -56,6 +150,57 @@ class Player(pg.sprite.Sprite):
         self.occupied_with_object('x')
         self.rect.y = self.pos.y
         self.occupied_with_object('y')
+        if self.vel.y == 0 and self.vel.x == 0 and not self.in_house:
+            self.animation = 'standing'
+            self.animate = True
+
+        if self.animate:
+            if self.animation == 'right':
+                self.__create_animation__(0.05, self.animation_right)
+
+            if self.animation == 'left':
+                self.__create_animation__(0.05, self.animation_left)
+
+            if self.animation == 'forward':
+                self.__create_animation__(0.05, self.animation_forward)
+
+            if self.animation == 'forward_right':
+                self.__create_animation__(0.05, self.animation_forward_right)
+
+            if self.animation == 'forward_left':
+                self.__create_animation__(0.05, self.animation_forward_left)
+
+            if self.animation == 'standing':
+                self.__create_animation__(0.03, self.animation_standing)
+
+            if self.animation == 'down_left':
+                self.__create_animation__(0.05, self.animation_down_left)
+
+            if self.animation == 'down_right':
+                self.__create_animation__(0.05, self.animation_down_right)
+
+            if self.animation == 'down':
+                self.__create_animation__(0.05, self.animation_down)
+
+            if self.animation == 'death':
+                self.__create_animation__(0.05, self.animation_death)
+
+            if self.animation == 'housing':
+                self.current_sprite += 0.15
+
+                if self.current_sprite >= len(self.housing_animation):
+                    self.current_sprite = len(self.housing_animation) - 1
+
+                self.image = self.housing_animation[int(self.current_sprite)]
+
+    def __create_animation__(self, speed, name):
+        self.current_sprite += speed
+
+        if self.current_sprite >= len(name):
+            self.current_sprite = 0
+            self.animate = False
+
+        self.image = name[int(self.current_sprite)]
 
 
 class Obstacle(pg.sprite.Sprite):
@@ -68,3 +213,27 @@ class Obstacle(pg.sprite.Sprite):
         self.y = y
         self.rect.x = x
         self.rect.y = y
+
+
+class Item(pg.sprite.Sprite):
+    def __init__(self, game, pos, type):
+        self._layer = ITEMS_LAYER
+        self.groups = game.all_sprites, game.items
+        pg.sprite.Sprite.__init__(self, self.groups)
+        self.game = game
+        self.image = game.item_images[type]
+        self.rect = self.image.get_rect()
+        self.type = type
+        self.rect.center = pos
+        self.pos = pos
+        self.tween = tween.easeInOutQuart
+        self.step = 0
+        self.direction = 1
+
+    def update(self):
+        offset = BOB_RANGE * (self.tween(self.step / BOB_RANGE) - 0.5)
+        self.rect.centery = self.pos[1] + offset * self.direction
+        self.step += BOB_SPEED
+        if self.step > BOB_RANGE:
+            self.step = 0
+            self.direction *= -1
